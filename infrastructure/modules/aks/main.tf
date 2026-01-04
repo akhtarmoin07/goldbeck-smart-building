c543b05e4edf9595fe42bcfecd079414144497a5
Moin Akhtar Choudhury <akhtarmoin07@gmail.com>
2026-01-04 14:48:36 +0100

    temporary name for rotation

--- infrastructure/modules/aks/main.tf
+++ infrastructure/modules/aks/main.tf
@@ -13,6 +13,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count = 1
     vm_size    = "Standard_B2s_v2"
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
+    temporary_name_for_rotation = "tempsystem"
   }
 
   identity {
